ff4994cb59d687852e8e15f08a5dd808e2225a15
rahulmula <rahulgoud.mula@amd.com>
2019-04-18 21:48:20 +0530

    initialcommit

--- main.py
+++ main.py
@@ -94,20 +94,23 @@ def delete():
 
 
 @app.route("/templateview", methods=['GET', 'POST'])
-def bkcview():
-    try:
-        db = mysql.connector.connect(host='localhost', user='rahul', passwd='Password@123', db='tcdata')
-        print("Connected successfully!!!")
-    except:
-        print("Could not connect to MongoDB")
+def templateview():
+    if request.method == 'GET':
+        try:
+            db = mysql.connector.connect(host='localhost', user='rahul', passwd='Password@123', db='tcdata')
+            print("Connected successfully!!!")
+        except:
+            print("Could not connect to MongoDB")
 
-    db = mysql.connector.connect(host='localhost', user='rahul', passwd='Password@123', db='tcdata')
-    cur = db.cursor()
-    cur.execute("SELECT bkcversion FROM tcbkcdata ORDER by Date ASC LIMIT 1;")
-    current=cur.fetchall()
-    cur.execute("SELECT TOP (2) bkcversion FROM tcbkcdata ORDER by Date ASC LIMIT 2;")
-    previous=cur.fetchall()
-    return render_template("template.html",crnt=current,prvs=previous)
+        db = mysql.connector.connect(host='localhost', user='rahul', passwd='Password@123', db='tcdata')
+        cur = db.cursor()
+        cur.execute("SELECT bkcversion FROM tcbkcdata ORDER by bkcdate ASC LIMIT 1;")
+        current=cur.fetchall()
+        cur.execute("SELECT bkcversion FROM tcbkcdata ORDER by bkcdate ASC LIMIT 1,1;")
+        previous=cur.fetchall()
+        return render_template("template.html",crnt=current,prvs=previous)
+    else:
+        return render_template('template.html')
 
 @app.route('/bkcadd', methods = ['POST', 'GET'])
 def bkcadd():
